9db25fd03fe220da741114a6bf76b19f24cf1f81
karthikpeddi14 <128394123+karthikpeddi14@users.noreply.github.com>
2023-07-16 20:46:56 +0530

    Update Unique Paths.cpp

--- Unique Paths.cpp
+++ Unique Paths.cpp
@@ -14,3 +14,29 @@ int uniquePaths(int m, int n) {
 	return ans(m,n,dp);
 }
 //recursion and memoization
+
+//tabulation
+#include <bits/stdc++.h> 
+// consider dp to be filled and it gives the answer for certain i,j
+
+int uniquePaths(int m, int n) {
+	
+	int dp[m][n];
+	for(int i=0 ;i<m ; i++)
+	{
+		for(int j=0 ; j<n ; j++)
+		{
+			if(i==0 && j==0) dp[i][j] = 1;
+			else
+			{
+				int right = 0;
+				int down = 0;
+				if(j>0) right = dp[i][j-1];
+				if(i>0) down = dp[i-1][j];
+				dp[i][j] = right+down;
+			}
+		}
+	}
+	return dp[m-1][n-1];
+}
+//tabulation
