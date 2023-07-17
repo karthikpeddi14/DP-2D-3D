53a44e23215a1403cb661be4e81a9448b444ca17
karthikpeddi14 <128394123+karthikpeddi14@users.noreply.github.com>
2023-07-17 15:17:32 +0530

    Update Triangle min path(striver standard).cpp

--- Triangle min path(striver standard).cpp
+++ Triangle min path(striver standard).cpp
@@ -26,3 +26,33 @@ public:
     }
 };
 //tabulation
+
+//space optimal
+class Solution {
+public:
+    
+    int minimumTotal(vector<vector<int>>& triangle) 
+    {
+        int n=triangle.size();
+        vector<int> next(n,0);
+
+        for(int i=0 ; i<n ; i++)
+        {
+            next[i] = triangle[n-1][i];
+        }
+
+        for(int i=n-2; i>=0 ; i--)
+        {
+            vector<int> cur(n,0);
+            for(int j=i ; j>=0 ; j--)
+            {
+                int d = next[j];
+                int dr = next[j+1];
+                cur[j] = min(d,dr) + triangle[i][j];
+            }
+            next = cur;
+        }
+        return next[0];
+    }
+};
+// space optimal
